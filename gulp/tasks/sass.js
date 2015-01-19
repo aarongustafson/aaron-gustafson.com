baf6372598e07810d72f7646252118ba571f2b3e
aarongustafson <aaron@easy-designs.net>
2015-01-19 14:06:44 -0500

    Site updated at 2015-01-19 19:06:44 UTC

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -7,7 +7,8 @@ var gulp = require('gulp'),
     path = require('path'),
     handleErrors = require('../utils/handleErrors'),
     source_folder = 'source/_styles/*.scss',
-    destination_folder = 'source/c';
+    destination_folder = 'source/c',
+    preview_folder = 'public/c';
 
 gulp.task('styles', function() {
     return gulp.src(source_folder)
@@ -17,9 +18,11 @@ gulp.task('styles', function() {
          }))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest(destination_folder))
+        .pipe(gulp.dest(preview_folder))
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
         .pipe(gulp.dest(destination_folder))
+        .pipe(gulp.dest(preview_folder))
         .pipe(notify({ message: 'Styles task complete' }))
         .on('error', handleErrors);;
 });
