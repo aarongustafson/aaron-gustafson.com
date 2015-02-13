5230a583cbfc96bf66e561c9083a21b83375a2de
aarongustafson <aaron@easy-designs.net>
2015-02-12 21:46:28 -0500

    updating the daemon

--- sh/publish.sh
+++ sh/publish.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-echo “Updating aaron-gustafson.com”
 cd /Users/Easy/Sites/aaron-gustafson.com/
-/usr/bin/git pull
-echo “Updated aaron-gustafson.com”
 
-echo “Publishing to aaron-gustafson.com”
-cd /Users/Easy/Sites/aaron-gustafson.com/
+echo "Updating aaron-gustafson.com"
+/usr/bin/git pull
+echo "Updated aaron-gustafson.com"
+
+echo "Moving new files into the repository"
+mv /Users/Easy/Dropbox/Blog/to_publish/*.markdown ./source/_posts
+echo "Done moving files"
+
+echo "Publishing to aaron-gustafson.com"
 /usr/bin/rake go
-echo “Publishing complete, check aaron-gustafson.com”
+echo "Publishing complete, check aaron-gustafson.com"
 
 curl http://textbelt.com/text -d number=4238381727 -d "message=Publishing complete, check aaron-gustafson.com"
