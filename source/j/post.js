919dafbb2a5ac06d87ac0be311a2dabeb37d3006
Aaron Gustafson <aaron@easy-designs.net>
2015-03-05 11:35:44 -0500

    Site updated at 2015-03-05 16:35:44 UTC

--- source/j/post.js
+++ source/j/post.js
@@ -1,3 +1,45 @@
+/* ! Sharing popup */
+(function( window, document ){
+	
+	// Filter older browsers
+	if ( ! 'querySelectorAll' in document )
+	{
+		return;
+	}
+	
+	// event handler
+	function click(e)
+	{
+		console.log('click');
+		var target = e.target;
+
+		// target must be an anchor and the inner width threshold must be met
+		if ( e.target.nodeName.toLowerCase() == 'a' &&
+			 window.innerWidth >= threshold )
+		{
+			// prevent the default link click
+			e.preventDefault();
+
+			// open the link in a popup
+			window.open( target.href, 'share-this', 'height=300,width=500,status=no,toolbar=no' );
+
+			// return
+			return false;
+		}
+	}
+
+		// gather the links container
+	var share_links = document.querySelectorAll('.entry__sharing'),
+		// set the threshold width
+		threshold = 640;
+
+	// watcher
+	if ( share_links.length > 0 )
+	{
+		share_links[0].addEventListener( 'click', click, false );
+	}
+
+}( this, this.document ));
 /**
  *  WebMentions.io JS
  *  A re-tooling of Aaron Parecki’s recommended JS for using the WebMention.io API
