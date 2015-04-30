4b25ed9193040cc47fffefcd87e2e6e4979a6758
Aaron Gustafson <aaron@easy-designs.net>
2015-04-30 16:07:52 -0400

    Site updated at 2015-04-30 20:07:52 UTC

--- plugins/adaptive_image.rb
+++ plugins/adaptive_image.rb
@@ -3,24 +3,32 @@
 # Adaptive Images with the Google Image Resizer
 # For more, see https://carlo.zottmann.org/2013/04/14/google-image-resizer/
 #
-# In _config.yml, include the following:
-#
-#  adaptive_image:
-#    cache: 2592000
-#    srcset: 
-#      - 1920
-#      - 600
-#      - 320
-#    sizes:
-#      - 
-#
-#  `cache` if for the length of time you want to cache it (in seconds)
-#  `srcset` is a list of sizes (in widths) you want to support
-#  `sizes` is the sizes attribute you want to offer
-#
-#   {% adaptive_image /path/to/image.jpg [attr="value"]* %}
-#
-# https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?url=http://news.microsoft.com/build2015/assets/photos/build720150428_web.png&resize_w=1080&container=focus&refresh=2592000
+# An implementation of Adaptive Images with `srcset` and `sizes` using [Google’s open image resizing service](https://carlo.zottmann.org/2013/04/14/google-image-resizer/).
+# 
+# The tag is simple:
+# 
+#   {% adaptive_image /path/to/image.jpg [attr="value"] %}
+# 
+# You can add as many attributes as you want.
+# 
+# ## Global Configuration
+# 
+# To keep things simple and consistant, you can set up the standard sizes of your adaptive images in variables in `_config.yml`:
+# 
+#   adaptive_image:
+#     cache: 2592000
+#     srcset: 
+#       - 1920
+#       - 600
+#       - 320
+#     sizes:
+#       - "(min-width:60em) 42.5em"
+#       - 100vw
+#   
+# * `cache` is for the length of time you want to cache it (in seconds)
+# * `srcset` is a list of sizes (in unitless pixel widths) you want to offer
+# * `sizes` is the size configurations for layout purposes
+# 
 
 module Jekyll
 
@@ -66,10 +74,16 @@ module Jekyll
           end
         }
 
+        # set up the url
+        original_source = markup[:image_src]
+        if original_source[0] == '/' and original_source[1] != '/'
+          original_source = "#{site.config['url']}#{original_source}"
+        end
+
         # Add the src & srcset
         srcset = []
         settings['srcset'].each do |size|
-          the_src = @url % {url: markup[:image_src], width: size, cache: settings['cache']}
+          the_src = @url % {url: original_source, width: size, cache: settings['cache']}
           the_src << " #{size}w"
           srcset << the_src
           if ! smallest_src or smalles_src > size
