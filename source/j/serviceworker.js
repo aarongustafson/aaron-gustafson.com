962fe7b39a6b44f4ffabd9ab9c5b1ebf2b37d428
Aaron Gustafson <aaron@easy-designs.net>
2016-02-24 21:31:38 -0500

    Site updated at 2016-02-25 02:31:38 UTC

--- source/j/serviceworker.js
+++ source/j/serviceworker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var version = 'v1455923081018:',
+var version = 'v1456367491253:',
 	default_avatar = 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mm&f=y',
 	missing_image = 'https://i.imgur.com/oWLuFAa.gif';
 self.addEventListener( 'activate', function( event ){
