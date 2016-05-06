d4f50da299cd932e30d808fd9e4d0939843acc0f
Aaron Gustafson <aaron@easy-designs.net>
2016-05-06 11:52:51 -0400

    Site updated at 2016-05-06 15:52:51 UTC

--- source/j/main.js
+++ source/j/main.js
@@ -394,17 +394,44 @@
     }
     
     var $search_link = document.querySelector( '.main-navigation__link--search' ),
-        $search_form = document.querySelector( '.form--search' );
+        $search_form = document.querySelector( '.form--search' ),
+        open = ( window.location.hash == '#search' );
     
     $search_form.classList.add( 'toggleable' );
+    $search_form.addEventListener( 'click', closeSearch, false );
+    $search_form.addEventListener( 'touchdown', closeSearch, false );
 
     $search_link.addEventListener( 'click', openSearch, false );
     $search_link.addEventListener( 'touchdown', openSearch, false );
     
+    // handle back
+    window.addEventListener('pageshow', function(event) {
+        open = ( window.location.hash == '#search' );
+    });
+    
     function openSearch( e ) {
+        if ( open ) { return false; }
+        open = true;
+        
         e.preventDefault();
         $search_form.classList.add( 'toggleable--open' );
         $search_form.querySelector( '[type=search]' ).focus();
+    }
+    
+    function closeSearch( e ) {
+        if ( ! open ) { return false; }
+        open = false;
+        
+        var target_tag = e.target.nodeName.toLowerCase();
+        if ( target_tag == 'button' ||
+             target_tag == 'input' ) {
+            return false;
+        } 
+        
+        e.preventDefault();
+        window.location.hash = '#';
+        $search_form.classList.remove( 'toggleable--open' );
+        $search_link.focus();
     }
     
 }(this,this.document));
--- source/j/serviceworker.js
+++ source/j/serviceworker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var version = 'v1460677310742:',
+var version = 'v1462549872652:',
 	default_avatar = 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mm&f=y',
 	missing_image = 'https://i.imgur.com/oWLuFAa.gif';
 self.addEventListener( 'activate', function( event ){
