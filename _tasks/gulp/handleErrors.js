f1c7b77aeaa604327fde6fcc94d3507ec35f060e
Aaron Gustafson <aaron@easy-designs.net>
2018-10-10 17:39:45 -0700

    Site generated at 2018-10-11 00:39:45 UTC

--- _tasks/gulp/handleErrors.js
+++ _tasks/gulp/handleErrors.js
@@ -1,15 +1,15 @@
-var notify = require("gulp-notify");
+var notify = require('gulp-notify');
 
-module.exports = function() {
+module.exports = function () {
 
-	var args = Array.prototype.slice.call(arguments);
+  var args = Array.prototype.slice.call(arguments);
 
-	// Send error to notification center with gulp-notify
-	notify.onError({
-		title: "Compile Error",
-		message: "<%= error.message %>"
-	}).apply(this, args);
+  // Send error to notification center with gulp-notify
+  notify.onError({
+    title: 'Compile Error',
+    message: '<%= error.message %>'
+  }).apply(this, args);
 
-	// Keep gulp from hanging on this task
-	this.emit('end');
+  // Keep gulp from hanging on this task
+  this.emit('end');
 };
