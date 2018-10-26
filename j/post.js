5f201971113428495fb62cf98ba97b5fdd3ee1ff
Aaron Gustafson <aaron@easy-designs.net>
2018-10-26 15:16:50 -0700

    Removed sibling anchors & re-generated JavaScript

--- j/post.js
+++ j/post.js
@@ -781,34 +781,6 @@ exports.Liquid=Liquid;}// @ts-check
             JekyllWebmentionIO.types = { 'bookmarks': 'bookmark','likes': 'like','links': 'link','posts': 'post','replies': 'reply','reposts': 'repost','rsvps': 'rsvp' };
           }(this, this.JekyllWebmentionIO));
 
-(function( window, document ){
-    'use strict';
-
-    if ( ! ( 'querySelectorAll' in document ) ) { return; }
-
-    var s = document.createElement('script');
-
-    // Preconnect to Disqus if possible
-    if ( 'AG' in window &&
-         'preconnect' in window.AG )
-    {
-        window.AG.preconnect( '//disqus.com/' );
-        window.AG.preconnect( '//aarongustafson.disqus.com' );
-        window.AG.preconnect( '//links.services.disqus.com/' );
-        window.AG.preconnect( '//a.disquscdn.com' );
-    }
-    else
-    {
-        //console.log('No preconnect');
-    }
-
-    // Add the comment count
-    s.async = true;
-    s.type = 'text/javascript';
-    s.src = '//aarongustafson.disqus.com/count.js';
-    (document.head || document.body).appendChild(s);
-
-}( this, this.document ));
 // Add a comments message if offline
 (function(){
     'use strict';
