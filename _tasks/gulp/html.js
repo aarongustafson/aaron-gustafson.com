866d20742bf7d05c3bbc377e4614b06e1ce4bd5d
Aaron Gustafson <aaron@easy-designs.net>
2018-11-01 14:30:19 -0700

    latest tweaks & updates

--- _tasks/gulp/html.js
+++ _tasks/gulp/html.js
@@ -1,17 +1,33 @@
-var gulp = require('gulp'),
-  htmlmin = require('gulp-htmlmin');
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
 
-gulp.task('html', function() {
+export function html() {
   var source = './_site/**/*.html',
-    destination = './_site',
-    htmlmin_config = {
-      removeComments: true,
-      collapseWhitespace: true,
-      minifyJS: true,
-      ignoreCustomFragments: [ /{%[\s\S]*?%}/, /{{[\s\S]*?}}/ ]
-    };
-
+      destination = './_site',
+      htmlmin_config = {
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyJS: true,
+        ignoreCustomFragments: [ /{%[\s\S]*?%}/, /{{[\s\S]*?}}/ ]
+      };
   return gulp.src(source)
-    .pipe(htmlmin(htmlmin_config))
-    .pipe(gulp.dest(destination));
-});
+           .pipe(htmlmin(htmlmin_config))
+           .pipe(gulp.dest(destination));
+}
+
+
+// var gulp = require('gulp'),
+//   htmlmin = require('gulp-htmlmin');
+// gulp.task('html', function() {
+//   var source = './_site/**/*.html',
+//     destination = './_site',
+//     htmlmin_config = {
+//       removeComments: true,
+//       collapseWhitespace: true,
+//       minifyJS: true,
+//       ignoreCustomFragments: [ /{%[\s\S]*?%}/, /{{[\s\S]*?}}/ ]
+//     };
+//   return gulp.src(source)
+//     .pipe(htmlmin(htmlmin_config))
+//     .pipe(gulp.dest(destination));
+// });
