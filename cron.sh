b1d6be943b23428381a4df7e9e8e27f063076d9a
Aaron Gustafson <aaron@easy-designs.net>
2019-02-08 11:37:42 -0800

    Updating webmentions

--- cron.sh
+++ cron.sh
@@ -3,19 +3,24 @@
 # Conver the .plist
 plutil -p /Users/aarongu/.MacOSX/environment.plist > /tmp/$EUID.vars
 
-# parse it and replace with exports
+# parse it and replace with variable exports
 ruby -p -i -e "gsub('{', '')" /tmp/$EUID.vars
 ruby -p -i -e "gsub('}', '')" /tmp/$EUID.vars
 ruby -p -i -e "gsub('  \"', 'export ')" /tmp/$EUID.vars
 ruby -p -i -e "gsub('\" => \"', '=')" /tmp/$EUID.vars
 ruby -p -i -e "gsub('\"', '')" /tmp/$EUID.vars
 
-# execute!
+# load in the ENV vars
 source /tmp/$EUID.vars
 
-# remove it
-#rm /tmp/$EUID.vars
+# remove the file
+rm /tmp/$EUID.vars
 
-cd /Users/aarongu/Dropbox/Sites/aaron-gustafson.com
+# Did we get the vars?
 # sudo -E -u aarongu printenv
+
+# go into this folder
+cd /Users/aarongu/Dropbox/Sites/aaron-gustafson.com
+
+# Make sure the ENV persists
 sudo -E -u aarongu rake go
