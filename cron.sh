ac410d6ebef4b6b8910274aa81561ed9c62965c5
Aaron Gustafson <aaron@easy-designs.net>
2019-02-07 20:43:29 -0800

    Site generated at 2019-02-08 04:43:29 UTC

--- cron.sh
+++ cron.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+
+# Conver the .plist
+plutil -p /Users/aarongu/.MacOSX/environment.plist > /tmp/$EUID.vars
+
+# parse it and replace with exports
+ruby -p -i -e "gsub('{', '')" /tmp/$EUID.vars
+ruby -p -i -e "gsub('}', '')" /tmp/$EUID.vars
+ruby -p -i -e "gsub('  \"', 'export ')" /tmp/$EUID.vars
+ruby -p -i -e "gsub('\" => \"', '=')" /tmp/$EUID.vars
+ruby -p -i -e "gsub('\"', '')" /tmp/$EUID.vars
+
+# execute!
+. /tmp/$EUID.vars
+
+# remove it
+rm /tmp/$EUID.vars
+
 cd /Users/aarongu/Dropbox/Sites/aaron-gustafson.com
+# printenv
 su aarongu rake go
