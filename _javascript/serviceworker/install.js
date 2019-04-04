6d270d0a19795a1ce092dc4be0c2a97036fc6183
Aaron Gustafson <aaron@easy-designs.net>
2019-04-04 15:11:54 -0700

    Site generated at 2019-04-04 22:11:54 UTC

--- _javascript/serviceworker/install.js
+++ _javascript/serviceworker/install.js
@@ -2,6 +2,9 @@ self.addEventListener( "install", function( event ){
 
   // console.log( "WORKER: install event in progress." );
 
+  // immediately take over
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open( sw_caches.static.name )
       .then(function( cache ){
--- j/serviceworker.js
+++ j/serviceworker.js
@@ -353,6 +353,9 @@ self.addEventListener( "install", function( event ){
 
   // console.log( "WORKER: install event in progress." );
 
+  // immediately take over
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open( sw_caches.static.name )
       .then(function( cache ){
