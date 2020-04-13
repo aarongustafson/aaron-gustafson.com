bc5def6c8130ace337d4177dd27ae5a24aa99cc7
Aaron Gustafson <aaron@easy-designs.net>
2020-04-13 13:38:43 -0700

    updating my SW registration

--- _javascript/main/register-serviceworker.js
+++ _javascript/main/register-serviceworker.js
@@ -3,23 +3,26 @@
   if ( "serviceWorker" in navigator )
   {
     window.sw_version = "v4:";
-    navigator.serviceWorker.register( "/serviceworker.min.js" );
+
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register( "/serviceworker.min.js" );
+
+      // Store page names & descriptions
+      if ( ! /\/notebook\/.+/.test( window.location ) )
+      {
+        var data = {
+          title: document.querySelector("[property='og:title']").getAttribute("content"),
+          description: document.querySelector( "meta[name='description']" ).getAttribute("content")
+        };
+        localStorage.setItem( location, JSON.stringify(data) );
+      }
+    });
 
     if ( navigator.serviceWorker.controller )
     {
       window.addEventListener( "load", function(){
         navigator.serviceWorker.controller.postMessage( "clean up" );
       });
-    }
-
-    // Store page names & descriptions
-    if ( ! /\/notebook\/.+/.test( window.location ) )
-    {
-      var data = {
-        title: document.querySelector("[property='og:title']").getAttribute("content"),
-        description: document.querySelector( "meta[name='description']" ).getAttribute("content")
-      };
-      localStorage.setItem( location, JSON.stringify(data) );
     }
 
   }
