22fda68fbb02df94a9b92fc8ced0980486e279eb
Aaron Gustafson <aaron@easy-designs.net>
2022-09-08 14:34:58 -0700

    Resolved SVG + gulp issues

--- _tasks/images.js
+++ _tasks/images.js
@@ -2,9 +2,11 @@
 const {dest, src} = require('gulp');
 const config = require("./config.js");
 const squoosh = require("gulp-squoosh");
+const svgo = require("gulp-svgo");
 const filter = require("gulp-filter");
-const through = require("through2");
-var path = require('path');
+const gulpif = require("gulp-if");
+const { Transform } = require("stream");
+var path = require("path");
 const fs = require("fs");
 
 const destination = `${config.static}/i`;
@@ -13,34 +15,49 @@ const dist = `${config.destination}/i`;
 // log
 const cache_file = `${config.cache}/optimized-images.json`;
 const optimized_images = JSON.parse(fs.readFileSync(cache_file));
-const cacheFiles = function(){
-  return through.obj(
-    function(file, enc, cb) {
+const cacheFiles = () => {
+  let stream = new Transform({
+    objectMode: true,
+    transform: (file, encoding, next) => {
       const file_path = path.relative(file.cwd, file.path);
       optimized_images.push(file_path);
-      cb();
+      return next(null, file);
+    }
+  });
+  return stream;
+};
+const writeCache = () => {
+  let stream = new Transform({
+    objectMode: true,
+    transform: (file, encoding, next) => {
+      return next(null, file);
     },
-    function(cb) {
+    flush: (next) => {
       fs.writeFileSync(cache_file, JSON.stringify(optimized_images, null, '  '));
-      cb();
-    });
+      return next();
+    }
+  });
+  return stream;
 };
 
 // Grabs all images, runs them through imagemin
 // and plops them in the dist folder
 const images = () => {
-
-  return src(`${config.source}/_images/**/*.{jpg,png,svg,gif}`)
+  
+  return src([`${config.source}/_images/**/*.{jpg,png,gif,svg}`, `!${config.source}/**/*.sketch/**`])
     // Only take new files
     .pipe(filter(function(file){
       return optimized_images.every(entry => !file.path.includes(entry));
     }))
-    // Save the list of new files to disk
+    // Save the list of new files
     .pipe( cacheFiles() )
     // Optimize
-    .pipe( squoosh() )
+    .pipe( gulpif( "*.svg", svgo(), squoosh() ) )
+    // Save
     .pipe( dest( destination ) )
     .pipe( dest( dist ) )
+    // Write the list of new files to disk
+    .pipe( writeCache() );
     //.pipe( gulpIf( /.*?\.(jpg|png)$/, webp() ) )
     //.pipe( dest(`${config.source}/static/i`) )
     //.on('done', cb);
