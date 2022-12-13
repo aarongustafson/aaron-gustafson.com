beb4479313d967f083dba5575c45d071d0935eb2
Aaron Gustafson <aaron@easy-designs.net>
2022-12-13 14:58:06 -0800

    Tweaking dark mode links

--- src/_javascript/main/register-serviceworker.js
+++ src/_javascript/main/register-serviceworker.js
@@ -2,7 +2,7 @@
   // Register the service worker
   if ( "serviceWorker" in navigator )
   {
-    window.sw_version = "v13:";
+    window.sw_version = "v14:";
 
     window.addEventListener('load', function() {
       navigator.serviceWorker.register( "/serviceworker.js" );
