57418a63826ace0d3e6a41d1a333b86a280538cf
Aaron Gustafson <aaron@easy-designs.net>
2023-03-01 09:57:25 -0800

    Bumping SW version

--- src/_javascript/main/register-serviceworker.js
+++ src/_javascript/main/register-serviceworker.js
@@ -2,7 +2,7 @@
   // Register the service worker
   if ( "serviceWorker" in navigator )
   {
-    window.sw_version = "v15:";
+    window.sw_version = "v16:";
 
     window.addEventListener('load', function() {
       navigator.serviceWorker.register( "/serviceworker.js" );
--- src/_javascript/serviceworker/_header.js
+++ src/_javascript/serviceworker/_header.js
@@ -1,4 +1,4 @@
-const version = "v15:", // be sure to update ../main/register-serviceworker.js too
+const version = "v16:", // be sure to update ../main/register-serviceworker.js too
 
       // Stuff to load on install
       fallback_avatar = "/i/fallbacks/avatar.svg",
