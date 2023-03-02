1de79ee5414b72946b4bd2cc57a91299a6e6650b
Aaron Gustafson <aaron@easy-designs.net>
2023-03-02 11:04:45 -0800

    Bump SW

--- src/_javascript/main/register-serviceworker.js
+++ src/_javascript/main/register-serviceworker.js
@@ -2,7 +2,7 @@
   // Register the service worker
   if ( "serviceWorker" in navigator )
   {
-    window.sw_version = "v16:";
+    window.sw_version = "v17:";
 
     window.addEventListener('load', function() {
       navigator.serviceWorker.register( "/serviceworker.js" );
