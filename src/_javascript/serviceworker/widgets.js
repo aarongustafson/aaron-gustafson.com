24ed4b4902fcf4b78041f3b55cf3f733495cb970
Aaron Gustafson <aaron@easy-designs.net>
2023-10-02 15:39:32 -0700

    Adding more logging to the widgets code in SW

--- src/_javascript/serviceworker/widgets.js
+++ src/_javascript/serviceworker/widgets.js
@@ -6,168 +6,176 @@ var widget_templates = {};
 
 async function registerPeriodicSync( widget )
 {
-  // if the widget is set up to auto-update…
-  if ( "update" in widget.definition ) {
-    registration.periodicSync.getTags()
-      .then( tags => {
-        // only one registration per tag
-        if ( ! tags.includes( widget.definition.tag ) ) {
-          periodicSync.register( widget.definition.tag, {
-              minInterval: widget.definition.update
-          });
-        }
-      });
-  }
-  return;
+	console.log( "should we register a periodic sync?", "update" in widget.definition );
+	// if the widget is set up to auto-update…
+	if ( "update" in widget.definition ) {
+		registration.periodicSync.getTags()
+			.then( tags => {
+				// only one registration per tag
+				if ( ! tags.includes( widget.definition.tag ) ) {
+					console.log( `registering a periodic sync for ${widget.definition.tag}` );
+					periodicSync.register( widget.definition.tag, {
+							minInterval: widget.definition.update
+					});
+				}
+			});
+	}
+	return;
 }
 
 async function  unregisterPeriodicSync( widget )
 {
-  // clean up periodic sync?
-  if ( widget.instances.length === 1 &&
-       "update" in widget.definition )
-  {
-    periodicSync.unregister( widget.definition.tag );
-  }
-  return;
+	// clean up periodic sync?
+	if ( widget.instances.length === 1 &&
+			 "update" in widget.definition )
+	{
+		console.log( `cleaning up periodic sync for ${widget.definition.tag}` );
+		periodicSync.unregister( widget.definition.tag );
+	}
+	return;
 }
 
 async function updateWidgets( host_id )
 {
-  const config = host_id ? { hostId: host_id }
-                         : { installed: true };
-  
-  let queue = [];
-  await widgets.matchAll( config )
-    .then(async widgetList => {
-      for (let i = 0; i < widgetList.length; i++) {
-        queue.push(updateWidget( widgetList[i] ));
-      }
-    });
-  await Promise.all(queue);
-  return;
+	console.log( `Updating all widgets in host ${host_id}`);
+	const config = host_id ? { hostId: host_id }
+												 : { installed: true };
+	
+	let queue = [];
+	await widgets.matchAll( config )
+		.then( async widgetList => {
+			for (let i = 0; i < widgetList.length; i++) {
+				queue.push(updateWidget( widgetList[i] ));
+			}
+		});
+	await Promise.all(queue);
+	return;
 }
 
 async function getTemplate( template_url )
 {
-  let template = "";
-  if ( template_url in widget_templates )
-  {
-    template = widget_templates[ template_url ];
-  }
-  else
-  {
-    template = await ( await fetch( template_url ) ).text();
-    widget_templates[ template_url ] = template;
-  }
+	console.log( `Getting template at ${template_url}` );
+	let template = "";
+	if ( template_url in widget_templates )
+	{
+		console.log( "We have it in the cache" );
+		template = widget_templates[ template_url ];
+	}
+	else
+	{
+		console.log( "We need to fetch it" );
+		template = await ( await fetch( template_url ) ).text();
+		widget_templates[ template_url ] = template;
+	}
 
-  return template;
+	return template;
 }
 
 async function initAllWidgets( widget )
 {
-  if ( ! self.widgets )
-  {
-    return;
-  }
+	if ( ! self.widgets )
+	{
+		return;
+	}
 
-  const template_url = "/w/waiting.ac.json";
-  const template = await getTemplate( template_url );
-  const data = "{}";
-  const all_widgets = await self.widgets.matchAll({
-    installable: true
-  });
-  
-  for ( widget of all_widgets ) {
-    await self.widgets.updateByTag( widget.definition.tag, {
-      template,
-      data
-    });
-  }
+	const template_url = "/w/waiting.ac.json";
+	const template = await getTemplate( template_url );
+	const data = "{}";
+	const all_widgets = await self.widgets.matchAll({
+		installable: true
+	});
+	
+	for ( widget of all_widgets ) {
+		console.log( `Updating ${widget.definition.tag} with payload`, { template, data } );
+		await self.widgets.updateByTag( widget.definition.tag, {
+			template,
+			data
+		});
+	}
 }
 
 async function initializeWidget( widget )
 {
-  await updateWidget( widget );
-  await registerPeriodicSync( widget );
+	await updateWidget( widget );
+	await registerPeriodicSync( widget );
 }
 
 async function updateWidget( widget )
 {
-  const template = await getTemplate( widget.definition.msAcTemplate );
-  const data = await ( await fetch( widget.definition.data ) ).text();
-  console.log( template, data );
-
-  await self.widgets.updateByTag(widget.definition.tag, {
-    template,
-    data
-  });
+	const template = await getTemplate( widget.definition.msAcTemplate );
+	const data = await ( await fetch( widget.definition.data ) ).text();
+	
+	console.log( `Updating ${widget.definition.tag} with payload`, { template, data } );
+	await self.widgets.updateByTag(widget.definition.tag, {
+		template,
+		data
+	});
 }
 
 async function uninstallWidget( widget )
 {
-  if ( widget.instances.length === 1 && "update" in widget.definition )
-  {
-    await self.registration.periodicSync.unregister( widget.definition.tag );
-  }
+	if ( widget.instances.length === 1 && "update" in widget.definition )
+	{
+		await self.registration.periodicSync.unregister( widget.definition.tag );
+	}
 }
 
 self.addEventListener("activate", (event) => {
-  console.log("initializing all widgets");
-  event.waitUntil(
-    initAllWidgets()
-  );
+	console.log("Initializing all widgets");
+	event.waitUntil(
+		initAllWidgets()
+	);
 });
 
 self.addEventListener("widgetinstall", function(event) {
-  console.log("installing", event.widget);
-  event.waitUntil(
-    initializeWidget( event.widget )
-  );
+	console.log( `Installing ${event.widget.tag}` );
+	event.waitUntil(
+		initializeWidget( event.widget )
+	);
 });
 
 self.addEventListener("widgetuninstall", function(event) {
-  console.log("uninstalling", event.widget);
-  event.waitUntil(
-    uninstallWidget( event.widget )
-  );
+	console.log( `Uninstalling ${event.widget.tag}` );
+	event.waitUntil(
+		uninstallWidget( event.widget )
+	);
 });
 
 self.addEventListener("widgetresume", function(event) {
-  console.log("resuming widget", event.widget);
-  event.waitUntil(
-    updateWidget( event.widget )
-  );
+	console.log( `Resuming ${event.widget.tag}` );
+	event.waitUntil(
+		updateWidget( event.widget )
+	);
 });
 
 self.addEventListener("widgetclick", function(event) {
 
-  const widget = event.widget;
-  const action = event.action;
-    
-  switch ( action ) {
-    
-    // Custom Actions
-    case "refresh":
-      console.log("Asking a widget to refresh itself");
-      event.waitUntil(
-        updateWidget( widget )
-      );
-      break;
-  }
+	const widget = event.widget;
+	const action = event.action;
+		
+	switch ( action ) {
+		
+		// Custom Actions
+		case "refresh":
+			console.log("Asking a widget to refresh itself");
+			event.waitUntil(
+				updateWidget( widget )
+			);
+			break;
+	}
 
 });
 
 self.addEventListener("periodicsync", event => {
 
-  const tag = event.tag;
-  
-  if ( "widgets" in self )
-  {
-    const widget = widgets.getByTag( tag );
-    if ( widget && "update" in widget.definition ) {
-      event.waitUntil( updateWidget( widget ) );
-    }
-  }
+	const tag = event.tag;
+	
+	if ( "widgets" in self )
+	{
+		const widget = widgets.getByTag( tag );
+		if ( widget && "update" in widget.definition ) {
+			event.waitUntil( updateWidget( widget ) );
+		}
+	}
 
 });
