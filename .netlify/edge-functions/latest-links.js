f4be0bec0986d070f34f4333b7076d8c624addd3
Aaron Gustafson <aaron@easy-designs.net>
2023-11-06 13:13:57 -0800

    Setting up server events on a 5 minute poll

--- .netlify/edge-functions/latest-links.js
+++ .netlify/edge-functions/latest-links.js
@@ -1,15 +1,27 @@
+const INTERVAL_MINUTES = 5;
+
 export default async function latestLinks() {
   let timerId;
+  let last_message;
   const body = new ReadableStream({
     start(controller) {
-      timerId = setInterval(() => {
-        const date = new Date().toUTCString();
-        const message = "deployed";
-        const msg = new TextEncoder().encode(
-          `data: ${JSON.stringify( { date, message } )}\r\n\r\n`
-        );
-        controller.enqueue(msg);
-      }, 1000);
+      timerId = setInterval(async function(){
+        let feed = await fetch( "https://www.aaron-gustafson.com/feeds/latest-links.json" );
+        let latest_links = await feed.json();
+        let updated = latest_links.items[0].date_published;
+        if ( updated &&
+             last_message != updated )
+        {
+          const date = updated;
+          const message = "deployed";
+          const msg = new TextEncoder().encode(
+            `data: ${JSON.stringify( { date, message } )}\r\n\r\n`
+          );
+          controller.enqueue(msg);
+          last_message = updated;
+        }
+        resolve("Updated");
+      }, ( INTERVAL_MINUTES * 60 * 1000 ) );
     },
     cancel() {
       if (typeof timerId === "number") {
