6685a1717f6db5aec3a3af72fb978ec15b5cd859
Aaron Gustafson <aaron@easy-designs.net>
2025-10-22 13:18:29 -0700

    Faster gulp (#122)

--- _tasks/scripts.js
+++ _tasks/scripts.js
@@ -1,35 +1,39 @@
 /* jshint node: true */
-import gulp from "gulp";
-const {dest, src} = gulp;
-import config from "./config.js";
 import fs from "fs";
-import path from "path";
-import merge from "merge-stream";
-import gulpIf from "gulp-if";
+import gulp from "gulp";
 import concat from "gulp-concat";
+import gulpIf from "gulp-if";
+import newer from "gulp-newer";
 import rename from "gulp-rename";
 import composer from "gulp-uglify/composer.js";
-import uglify from "uglify-es";
-let minify = composer(uglify, console);
-import through2 from "through2";
+import merge from "merge-stream";
+import path from "path";
 import stream from "stream";
+import through2 from "through2";
+import uglify from "uglify-es";
+import config from "./config.js";
+const {dest, src} = gulp;
 const { Transform } = stream;
+
+let minify = composer(uglify, console);
 
 const source_folder = `${config.source}/_javascript`;
 const destination_folder = `${config.static}/j`;
 const dist = `${config.destination}/j`;
 const isWatch = process.argv.includes('watch');
 
-function getFolders( dir ) {
+function getFolders(dir) {
   return fs.readdirSync(dir)
-      .filter(function(file) {
-        return fs.statSync(path.join(dir, file)).isDirectory();
-      });
+    .filter(function(file) {
+      return fs.statSync(path.join(dir, file)).isDirectory();
+    });
 }
 
-// Update SW
+// Update SW with debouncing
+let swUpdateTimeout;
 const sw_data_file = `${config.source}/_data/sw.json`;
 const sw_data = JSON.parse(fs.readFileSync(sw_data_file));
+
 const updateServiceWorker = () => {
   let stream = new Transform({
     objectMode: true,
@@ -37,47 +41,73 @@ const updateServiceWorker = () => {
       return next(null, file);
     },
     flush: (next) => {
-			if ( isWatch ) {
-				sw_data.version = new Date().getTime();
-				fs.writeFileSync(sw_data_file, JSON.stringify(sw_data, null, '  '));
-			}
+      if (isWatch) {
+        // Debounce SW updates
+        clearTimeout(swUpdateTimeout);
+        swUpdateTimeout = setTimeout(() => {
+          sw_data.version = new Date().getTime();
+          fs.writeFileSync(sw_data_file, JSON.stringify(sw_data, null, 2));
+        }, 1000);
+      }
       return next();
     }
   });
   return stream;
 };
 
+// Optimized scripts function with better caching
 const scripts = cb => {
-  const folders = getFolders( source_folder );
+  const folders = getFolders(source_folder);
 
-  const tasks = folders.map( folder => {
-    return src( path.join( source_folder, folder, "*.js") )
-    // make a file from the folder
-    .pipe( concat(folder + ".js") )
-    // service worker goes to a different destination
-    .pipe(
-      gulpIf(
-        folder == "serviceworker",
-        through2.obj((file, _, cb) => {
-					cb();
-					return;
-				})
+  const tasks = folders.map(folder => {
+    const folderPath = path.join(source_folder, folder);
+    const outputFile = folder + ".js";
+    const outputMin = folder + ".min.js";
+    
+    // Check if any source files are newer than output
+    const destPath = path.join(destination_folder, outputFile);
+    
+    return src(path.join(folderPath, "*.js"))
+      // Only process if source files are newer than destination
+      .pipe(newer(destPath))
+      
+      // Concatenate files
+      .pipe(concat(outputFile))
+      
+      // Skip service worker for regular output
+      .pipe(
+        gulpIf(
+          folder === "serviceworker",
+          through2.obj((file, _, cb) => {
+            cb(); // Skip this file
+            return;
+          })
+        )
       )
-    )
-    // write expanded version
-    .pipe( dest(destination_folder) )
-    .pipe( dest(dist) )
-    // make & write minified version
-    .pipe( rename({suffix: ".min"} ) )
-    .pipe( minify() )
-    .pipe( dest(destination_folder) )
-    .pipe( dest(dist) )
-		.pipe( updateServiceWorker() )
-    .on('done', cb);
+      
+      // Write expanded version
+      .pipe(dest(destination_folder))
+      .pipe(dest(dist))
+      
+      // Create minified version
+      .pipe(rename({ suffix: ".min" }))
+      .pipe(minify({
+        compress: {
+          drop_console: !isWatch // Remove console.log in production
+        }
+      }))
+      .pipe(dest(destination_folder))
+      .pipe(dest(dist))
+      .pipe(updateServiceWorker());
   });
 
-  return merge(tasks);
-
+  const mergedTasks = merge(tasks);
+  
+  if (cb) {
+    mergedTasks.on('end', cb);
+  }
+  
+  return mergedTasks;
 };
 
 export default scripts;
